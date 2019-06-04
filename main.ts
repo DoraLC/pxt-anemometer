c747a70a0843c8508dcbb72604922ef38280ab0d
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-06-04 16:51:40 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ namespace anemometer{
      * Return wind speed in m/s.
      * @param t time interval; eg: 1, 2, 3, 5, 10
      */
-    //%block="Wind Speed %t"
+    //%block="Wind Speed time interval (s) %t "
     export function speed(t: number): number {
         n = 0;
         basic.pause(t * 1000);
