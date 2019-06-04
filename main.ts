2346112ad6c845a2e54edbdc898dbe4973db3151
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-06-04 18:04:37 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 //%color=#50F72E icon="\uf121" block="anemometer"
-namespace anemometer{
+namespace anemometer {
     const pi: number = 3.14;
     const r: number = 0.075;
     const N: number = 20;
 
     let n: number = 0;
     let v: number = 0;
+    let pin_arg: DigitalPin = DigitalPin.P2;
 
     function cal(t: number) {
         const threshold: number = 1.3;
@@ -17,15 +18,16 @@ namespace anemometer{
      * Return wind speed in m/s.
      * @param t time interval; eg: 1, 2, 3, 5, 10
      */
-    //%block="Wind Speed time intervals(s) %t "
-    export function speed(t: number): number {
+    //%block="Wind Speed time intervals(s) %t | pin %pinArg"
+    export function speed(t: number, pinArg: DigitalPin): number {
+        pin_arg = pinArg
         n = 0;
         basic.pause(t * 1000);
         cal(t);
         return v;
     }
 
-    pins.onPulsed(DigitalPin.P2, PulseValue.High, function () {
+    pins.onPulsed(pin_arg, PulseValue.High, function () {
         n++;
     })
 }
